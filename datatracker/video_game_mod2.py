4023bd9008c86c48a8e72c06409a3e406ad341a3
kyledobash <kyledobash@yahoo.com>
2021-02-24 10:39:43 -0700

    added first test view

--- datatracker/video_game_mod2.py
+++ datatracker/video_game_mod2.py
@@ -5,10 +5,25 @@ import requests
 
 bp = Blueprint('video_game_mod2', __name__)
 
+
 @bp.route('/api_test')
 def test():
     api_response = requests.get('https://api.dccresource.com/api/games/')
     # games = api_response.json()
     games = json.loads(api_response.content, object_hook=lambda d: SimpleNamespace(**d))
-    for game in games:
-        print(game)
+    return render_template('video_games_views/api_test.html', games=games)
+
+
+@bp.route('/best_console')
+def best_console():
+    pass
+
+
+@bp.route('/search_result')
+def search_result():
+    pass
+
+
+@bp.route('/custom_question')
+def custom_question():
+    pass
