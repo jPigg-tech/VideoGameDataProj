708d8e1db146ddcd2e0918fad972def6e1fd99ed
unknown <chue.vang0@gmail.com>
2021-02-25 13:42:37 -0600

    Created a search result view.

--- datatracker/video_game_mod2.py
+++ datatracker/video_game_mod2.py
@@ -64,26 +64,23 @@ def best_console():
     return render_template('video_games_views/best_console.html', platform_totals=platform_totals)
 
 
-@bp.route('/search_result')
-def search_result():
-    search_input =
-
+@bp.route('/search_result', methods = ['POST', 'GET'])
+def search_result(searchgame):
     api_response = requests.get('https://api.dccresource.com/api/games/')
-
     games = json.loads(api_response.content, object_hook=lambda d: SimpleNamespace(**d))
 
-    search_match = None
-
-    for game in games:
-        if game.name == search_input:
-            search_match = game
+    if search_result.method == 'POST':
 
 
 
 
+    # search_match = None
 
+    # for game in games:
+    #     if game.name == search_input:
+    #         search_match = game
 
-
+    return render_template('video_games_views/search_result.html')
 
 
 @bp.route('/custom_question')
