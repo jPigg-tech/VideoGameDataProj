32c0ffbdc6a1d8affdb553e71ab85d781483ecb0
kyledobash <kyledobash@yahoo.com>
2021-02-25 08:21:00 -0700

    completed filter for best console data

--- datatracker/video_game_mod2.py
+++ datatracker/video_game_mod2.py
@@ -28,6 +28,41 @@ def best_console():
         if game.year is not None and game.year >= 2013:
             recent_games.append(game)
 
+    platform_totals = {
+        'PS3': 0,
+        'PS4': 0,
+        'X360': 0,
+        'XOne': 0,
+        'PC': 0,
+        'Wii': 0,
+        'WiiU': 0,
+        '3DS': 0,
+        'PSV': 0,
+        'PSP': 0
+    }
+
+    for game in recent_games:
+        if game.platform == 'PS3':
+            platform_totals['PS3'] += game.globalSales
+        elif game.platform == 'PS4':
+            platform_totals['PS4'] += game.globalSales
+        elif game.platform == 'X360':
+            platform_totals['X360'] += game.globalSales
+        elif game.platform == 'XOne':
+            platform_totals['XOne'] += game.globalSales
+        elif game.platform == 'PC':
+            platform_totals['PC'] += game.globalSales
+        elif game.platform == 'Wii':
+            platform_totals['Wii'] += game.globalSales
+        elif game.platform == 'WiiU':
+            platform_totals['WiiU'] += game.globalSales
+        elif game.platform == '3DS':
+            platform_totals['3DS'] += game.globalSales
+        elif game.platform == 'PSV':
+            platform_totals['PSV'] += game.globalSales
+        elif game.platform == 'PSP':
+            platform_totals['PSP'] += game.globalSales
+
     return render_template('video_games_views/best_console.html', recent_games=recent_games)
 
 
